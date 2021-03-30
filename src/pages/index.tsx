44222fcebe0ee4e7e7c4e7958c2cda3e62601c4c
Jesse Jonas <jessejonas13@gmail.com>
2021-03-30 09:33:15 -0300

    feat: configurar SSR na home | configurar stripe

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,20 @@
+import { GetServerSideProps } from 'next';
+
 import Head from 'next/head';
 import { SubscribeButton } from '../components/SubscribeButton';
 
+import { stripe } from '../services/stripe';
+
 import styles from '../styles/home.module.scss';
 
-export default function Home() {
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  }
+}
+
+export default function Home({ product }: HomeProps) {
   return (
     <>
       <Head>
@@ -16,14 +27,37 @@ export default function Home() {
           <h1>News about the <span>React</span> world.</h1>
           <p>
             Get access to all publications <br />
-            <span>for $9.99 month</span>
+            <span>for {product.amount} month</span>
           </p>
 
-          <SubscribeButton />
+          <SubscribeButton priceId={product.priceId} />
         </section>
 
         <img src="/images/avatar.svg" alt="Girl coding"/>
       </main>
     </>
   )
+}
+
+// importar tipagem para essa function
+export const getServerSideProps: GetServerSideProps = async () => {
+  // todo codigo colocado aqui é executado no servidor node do next
+
+  const price = await stripe.prices.retrieve('price_1IagBhKMEMKyzCEgCtXsf6VL', {
+    expand: ['product'], // usado para ver info do produto
+  });
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-us', {
+      style: 'currency',
+      currency: 'USD',
+    }).format( (price.unit_amount / 100)), // centavos
+  };
+
+  return {
+    props: {
+      product,
+    }
+  }
 }
